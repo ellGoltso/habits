ccf8d23967eb4284215e4c4917ce61dbd0a4ab3e
ellGoltso <goltsovi3@gmail.com>
2026-01-12 18:31:34 +0300

    delete username

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
 
-admin.site.register(User, UserAdmin)
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    ordering = ('email',)
+    list_display = ('email', 'is_staff', 'is_active')
+    search_fields = ('email',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Персональная информация', {'fields': ('bio', 'avatar', 'phone', 'telegram_id')}),
+        ('Права доступа', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Важные даты', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password'),
+        }),
+    )
--- users/models.py
+++ users/models.py
@@ -1,12 +1,27 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
 
-class User(AbstractUser):
-    """
-    Кастомная модель пользователя.
-    """
+class UserManager(BaseUserManager):
+    """Менеджер для пользователя."""
 
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Email обязателен")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    username = None
     email = models.EmailField(
         "email address",
         unique=True,
@@ -14,21 +29,22 @@ class User(AbstractUser):
             "unique": "Пользователь с таким email уже существует.",
         },
     )
+
     bio = models.TextField("О себе", max_length=500, blank=True, null=True)
-    avatar = models.ImageField(
-        "Аватар", upload_to="users/avatars/", blank=True, null=True
-    )
+    avatar = models.ImageField("Аватар", upload_to="users/avatars/", blank=True, null=True)
     phone = models.CharField("Номер телефона", max_length=15, blank=True, null=True)
     telegram_id = models.CharField(
-        max_length=20, blank=True, null=True, verbose_name="ID чата в Telegram"
+        max_length=20, blank=True, null=True, verbose_name="ID чата в Telegram", db_index=True
     )
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["username"]
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
 
     class Meta:
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
 
     def __str__(self):
-        return self.username
+        return self.email
